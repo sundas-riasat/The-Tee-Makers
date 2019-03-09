d3a866ec10590d10ad5ba81da200bd397f9dea58
Sundas1997 <sundasriasat@gmail.com>
2019-03-09 17:57:11 +0500

    Angular Folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
+import {RouterModule} from '@angular/router';
+import { PreFooterComponent } from './pre-footer/pre-footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ShopComponent,
+    ContactComponent,
+    PreFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
