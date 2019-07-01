dcd8e6d9269309be753883b4a6c2729f1f9b138f
Sundas Riasat <sundasriasat@gmail.com>
2019-07-02 00:11:42 +0500

    All Good Finally

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '../../../node_modules/@angular/router';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+//components
+
+import {LoginComponent} from './login/login.component';
+
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardModule,
+    RouterModule.forChild([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'dashboard',
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+      }
+    ])
   ]
 })
 export class AdminModule { }
